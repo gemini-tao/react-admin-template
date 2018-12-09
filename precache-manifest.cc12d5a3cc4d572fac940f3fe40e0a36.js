a00fb979c01733cb0a13131c6cbac56c275024d1
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-12-09 12:20:26 +0000

    Deploy lf7817/react-admin-template to github.com/lf7817/react-admin-template.git:gh-pages

---
+++ precache-manifest.cc12d5a3cc4d572fac940f3fe40e0a36.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "90c22e533cd726076b5d",
+    "url": "./static/js/3.90c22e53.chunk.js"
+  },
+  {
+    "revision": "a981ab83faa9b089a2ef",
+    "url": "./static/css/main.64ba6c2d.chunk.css"
+  },
+  {
+    "revision": "8c02ab9750e5a048a09b",
+    "url": "./static/css/test.f47b403b.chunk.css"
+  },
+  {
+    "revision": "8c02ab9750e5a048a09b",
+    "url": "./static/js/test.8c02ab97.chunk.js"
+  },
+  {
+    "revision": "8578d7cb52fca59760ef",
+    "url": "./static/js/Demo.8578d7cb.chunk.js"
+  },
+  {
+    "revision": "990ae4d0c56d16d2ffdab6d924b6a179",
+    "url": "./static/media/500.990ae4d0.svg"
+  },
+  {
+    "revision": "a981ab83faa9b089a2ef",
+    "url": "./static/js/main.a981ab83.chunk.js"
+  },
+  {
+    "revision": "7ab44b60416f42ad136a",
+    "url": "./static/js/runtime~main.7ab44b60.js"
+  },
+  {
+    "revision": "2dda86ad45caee58c927e111fe3c8071",
+    "url": "./static/media/403.2dda86ad.svg"
+  },
+  {
+    "revision": "9ed13bbe02debb8e23db19a4e6603c2e",
+    "url": "./static/media/404.9ed13bbe.svg"
+  },
+  {
+    "revision": "90c22e533cd726076b5d",
+    "url": "./static/css/3.accddbb6.chunk.css"
+  },
+  {
+    "revision": "120285a6e53be41c2236e73ebfa2c20f",
+    "url": "./index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-importScripts("./precache-manifest.519817e050ba13dc35eca4bb1f3d1272.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("./precache-manifest.cc12d5a3cc4d572fac940f3fe40e0a36.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 /* eslint-disable */
 workbox.clientsClaim();
