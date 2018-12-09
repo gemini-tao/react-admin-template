6c81dfeff0d94575ba52ca1e2b6c7e54f2d8803b
lf7817 <535536456@qq.com>
2018-12-09 20:17:49 +0800

    添加meta 兼容苹果低版本浏览器

--- src/pages/Test/index.js
+++ src/pages/Test/index.js
@@ -9,7 +9,7 @@ class Test extends Component {
     console.log('render');
     return (
       <div className="aaa">
-        demo119
+        demo120
         {/* <RouterView routes={this.props.routes} /> */}
       </div>
     )
