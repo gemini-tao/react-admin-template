330c1c1171014e492286abc1d68beaf931e83810
lf7817 <535536456@qq.com>
2018-12-09 19:07:10 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-importScripts("./precache-manifest.daffc017e76a7a1a676165b0c757bc61.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("./precache-manifest.519817e050ba13dc35eca4bb1f3d1272.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 /* eslint-disable */
 workbox.clientsClaim();
