fc680691c8cf31ab21c7c71ba315341e8dc75065
lf7817 <535536456@qq.com>
2018-12-10 10:42:55 +0800

    修改通知布局

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
  * @Author: lifan
  * @Date: 2018-10-26 14:51:40
  * @Last Modified by: lifan
- * @Last Modified time: 2018-12-07 17:31:31
+ * @Last Modified time: 2018-12-10 10:22:50
  */
 /* eslint-disable import/no-extraneous-dependencies */
 import '@babel/polyfill';
@@ -11,7 +11,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { getPersistor } from '@rematch/persist';
-import { Button, notification } from 'antd';
+import { Button, notification, Icon } from 'antd';
 import { PersistGate } from 'redux-persist/es/integration/react';
 import store from './store';
 import RootRouter from './router';
@@ -43,14 +43,16 @@ function update (registration) {
 const openNotification = (registration) => {
   const key = `open${Date.now()}`;
   const btn = (
-    <Button type="primary" size="small" onClick={() => update(registration)}>
+    <Button type="primary" onClick={() => update(registration)}>
       升级
     </Button>
   );
   notification.open({
     message: '发现新版本',
-    description: '点击升级',
+    description: '本次升级添加了新功能，修复了一些bug,为了您更好的体验，请点击升级',
     duration: null,
+    placement: 'bottomRight',
+    icon: <Icon type="smile" theme="twoTone" style={{ color: '#108ee9' }} />,
     btn,
     key,
   });
