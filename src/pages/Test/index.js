4edd3bea7fc7c12a590cb505c900e22104a0cc5d
lf7817 <535536456@qq.com>
2018-12-11 09:57:49 +0800

    upgrade react-redux to v6

--- src/pages/Test/index.js
+++ src/pages/Test/index.js
@@ -9,7 +9,7 @@ class Test extends Component {
     console.log('render');
     return (
       <div className="aaa">
-        demo124
+        demo125
         {/* <RouterView routes={this.props.routes} /> */}
       </div>
     )
